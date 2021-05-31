36cf0c75044d899d47599693f3226f64799a4d43
Santhosh412 <beerellisanthosh412@gmail.com>
2021-05-31 08:18:25 +0530

    basics

---
+++ 19-04-2021/Multiple.java
@@ -0,0 +1,10 @@
+package com.ass19042021;
+
+public class Multiple {
+	
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		System.out.println((n / 100 + 1) * 100);
+	}
+
+}
