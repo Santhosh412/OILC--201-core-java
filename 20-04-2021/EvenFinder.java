7dab77935d18206210e126e0cd3232dba08debb7
Santhosh412 <beerellisanthosh412@gmail.com>
2021-05-31 08:30:54 +0530

    using methods

---
+++ 20-04-2021/EvenFinder.java
@@ -0,0 +1,18 @@
+package com.ass2;
+
+public class EvenFinder {
+	
+	static String isCheck(int num) {
+		return "" + (num % 2 == 0);
+	}
+	public static void main(String[] args) {
+		try {
+			int num = Integer.parseInt(args[0]);
+			System.out.println(isCheck(num));
+		}
+		catch(Exception e) {
+			System.out.println("Error");
+		}
+	}
+
+}
---
+++ 20-04-2021/Multiple.java
@@ -0,0 +1,14 @@
+package com.ass2;
+
+public class Multiple {
+	
+	static String multiple(int n) {
+		n = (n / 100 + 1) * 100;
+		return "" + n;
+	}
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		System.out.println(multiple(n));
+	}
+
+}
